57466976147a8dba1b2c595976582fdf95c73bb0
datuchela <davit.narimanidze408@gmail.com>
2022-11-29 18:24:36 +0400

    synced up index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import usePets from "../hooks/usePets";
 
 // UI Components
 import Link from "../components/atoms/Link";
+import Heading from "../components/atoms/Heading";
 
 const HomePage = () => {
   const { auth } = useAuth();
@@ -10,21 +11,34 @@ const HomePage = () => {
 
   return (
     <>
-      <div>hello, {auth?.user?.username}</div>
+      <div className="flex text-4xl">
+        Hi, <Heading>{auth?.user?.username}</Heading>
+      </div>
       <div>
-        <h2>Your pets:</h2>
         {isLoading ? (
           "loading..."
         ) : (
-          <ul>
-            {pets?.map((pet) => {
-              return (
-                <li key={pet.id}>
-                  <Link to={`/pet/${pet.id}`}>{pet.name}</Link>
-                </li>
-              );
-            })}
-          </ul>
+          <div>
+            {pets![0] ? (
+              <>
+                <h2>Your pets:</h2>
+                <ul>
+                  {pets?.map((pet) => {
+                    return (
+                      <li key={pet.id}>
+                        <Link to={`/pet/${pet.id}`}>{pet.name}</Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </>
+            ) : (
+              <div className="flex flex-col items-center">
+                <h2>You have no pets yet</h2>
+                <Link to="/add/pet">Add Pet</Link>
+              </div>
+            )}
+          </div>
         )}
       </div>
     </>
