91ad39a93551f9cf187bda4ded1bfe5472b73e2a
datuchela <davit.narimanidze408@gmail.com>
2022-11-29 22:07:31 +0400

    added ComboBox

--- src/components/atoms/ComboBox.tsx
+++ src/components/atoms/ComboBox.tsx
@@ -1,32 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Combobox } from "@headlessui/react";
 
 type ComboBoxPropsType = {
+  isLoading: boolean;
   arrayOfData: Array<{ id: number; name: string }>;
+  value: string[];
+  handleChange: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
 };
 
-const ComboBox: React.FC<ComboBoxPropsType> = ({ arrayOfData }) => {
-  const [selectedPerson, setSelectedPerson] = useState(arrayOfData[0]);
-  const [query, setQuery] = useState<string>("");
+const ComboBox: React.FC<ComboBoxPropsType> = ({ isLoading, arrayOfData, value, handleChange }) => {
+  const [selected, setSelected] = useState(arrayOfData ? arrayOfData[0].id : 0);
+  const [query, setQuery] = useState("");
 
-  const filteredArrayOfData =
+  useEffect(() => {
+    console.log(isLoading);
+  }, [isLoading]);
+
+  const filteredArray =
     query === ""
       ? arrayOfData
-      : arrayOfData.filter((entry) => {
+      : arrayOfData?.filter((entry) => {
           return entry.name.toLowerCase().includes(query.toLowerCase());
         });
 
   return (
-    <Combobox value={selectedPerson} onChange={setSelectedPerson}>
-      <Combobox.Input onChange={(event) => setQuery(event.target.value)} />
-      <Combobox.Options>
-        {filteredArrayOfData.map((entry) => (
-          <Combobox.Option key={entry.id} value={entry.id}>
-            {entry.name}
-          </Combobox.Option>
-        ))}
-      </Combobox.Options>
-    </Combobox>
+    <div className="border border-black">
+      <Combobox value={selected} onChange={setSelected}>
+        <Combobox.Input onChange={(event) => setQuery(event.target.value)} />
+        <Combobox.Options>
+          {filteredArray?.map((entry) => (
+            <Combobox.Option key={entry.id} value={entry.id}>
+              {entry.name}
+            </Combobox.Option>
+          ))}
+        </Combobox.Options>
+      </Combobox>
+    </div>
   );
 };
 
